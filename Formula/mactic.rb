072950b2a36c7db39fb555ec9fe64f47273875e5
Mateus Mercer <mateusmercer@gmail.com>
2026-04-06 17:41:23 -0300

    Update formula sha256 for v2.0

--- Formula/mactic.rb
+++ Formula/mactic.rb
@@ -3,7 +3,7 @@ class Mactic < Formula
   homepage "https://github.com/MatMercer/mactic"
   url "https://github.com/MatMercer/mactic/releases/download/v2.0/mactic-macos.tar.gz"
   version "2.0"
-  sha256 "TODO"
+  sha256 "e30c7522c9e526fb22bbe693aecf1a51e7ee651f5f7d87f03058627fd312f85f"
   license "Unlicense"
 
   depends_on :macos
